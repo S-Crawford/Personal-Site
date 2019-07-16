3c473ab933e07548238482da268212de88ba194f
The-Steve <stevenpcrawford@gmail.com>
2019-07-15 21:26:38 -0400

    Initial commit

---
+++ js/navbarViewModel.js
@@ -0,0 +1,9 @@
+const homeButton = document.querySelector(".home-button");
+const resumeButton = document.querySelector(".resume-button");
+const coverLetterButton = document.querySelector(".cover-letter-button");
+
+function isItWorking(){
+homeButton.textContent("TEST");
+}
+
+isItWorking();
